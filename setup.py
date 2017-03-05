162d50132b8c720db2957445835b1de4350f2d64
henocdz <henocdz@gmail.com>
2017-03-05 16:51:34 -0600

    PyPi basic files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='Work On',
+    version='1.0.0',
+    description='Switch between projects quickly from command line',
+    long_description='...',
+    keywords='terminal side projects manager work cli',
+    url='https://github.com/henocdz/workon',
+    download_url = 'https://github.com/henocdz/workon/tarball/1.0.0',
+    author='henocdz',
+    author_email='self@henocdz.com',
+    license='MIT',
+    packages=['workon'],
+    install_requires=['peewee==2.8.8'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
+)
