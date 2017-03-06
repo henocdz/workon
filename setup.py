05820707267faaf322de2149d31c7951936b9ca3
henocdz <henocdz@gmail.com>
2017-03-06 00:45:26 -0600

    Add workon.sh as script

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='Workon',
-    version='0.6.3',
+    version='0.7.0',
     description='Switch between projects quickly from command line',
     long_description='...',
     keywords='terminal side projects manager work cli',
     url='https://github.com/henocdz/workon',
-    download_url = 'https://github.com/henocdz/workon/tarball/0.6.3',
+    download_url = 'https://github.com/henocdz/workon/tarball/0.7.0',
     author='henocdz',
     author_email='self@henocdz.com',
     license='MIT',
     packages=['workon'],
+    scripts=['scripts/workon.sh'],
     install_requires=['peewee==2.8.8', 'fire==0.1.0'],
     classifiers=[
         'Intended Audience :: Developers',
