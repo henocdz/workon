16f191ee8a4f8f44b3523408f443c4423f5231b8
henocdz <henocdz@gmail.com>
2017-03-06 00:39:23 -0600

    Remove scripts from setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup
 
 setup(
     name='Workon',
-    version='0.6.2',
+    version='0.6.3',
     description='Switch between projects quickly from command line',
     long_description='...',
     keywords='terminal side projects manager work cli',
     url='https://github.com/henocdz/workon',
-    download_url = 'https://github.com/henocdz/workon/tarball/0.6.2',
+    download_url = 'https://github.com/henocdz/workon/tarball/0.6.3',
     author='henocdz',
     author_email='self@henocdz.com',
     license='MIT',
     packages=['workon'],
-    scripts=['scripts/work'],
     install_requires=['peewee==2.8.8', 'fire==0.1.0'],
     classifiers=[
         'Intended Audience :: Developers',
