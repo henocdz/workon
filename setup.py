46f1f6dc4ea95d8efd10adf93a06737237a6874d
henocdz <henocdz@gmail.com>
2017-09-20 22:08:50 -0500

    update fire dependencie to v0.1.2 to make iPython optional

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 setup(
     name='Workon',
-    version='0.8.1',
+    version='0.8.2',
     description='Switch between projects quickly from command line',
     long_description='...',
     keywords='terminal side projects manager work cli',
     url='https://github.com/henocdz/workon',
-    download_url = 'https://github.com/henocdz/workon/tarball/0.8.1',
+    download_url = 'https://github.com/henocdz/workon/tarball/0.8.2',
     author='henocdz',
     author_email='self@henocdz.com',
     license='MIT',
     packages=['workon'],
     scripts=['scripts/workon.sh'],
-    install_requires=['peewee==2.8.8', 'fire==0.1.0', 'six'],
+    install_requires=['peewee==2.8.8', 'fire==0.1.2', 'six'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
